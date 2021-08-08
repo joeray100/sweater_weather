28de1256313c723f88fb26f3cc5f0d5541fca82b
Joe Ray <jray100.jr@gmail.com>
2021-08-08 00:01:32 -0600

    Update routes for users & sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'backgrounds#show'
+      resources :sessions, only: :create
+      resources :users, only: :create
     end
   end
 end
