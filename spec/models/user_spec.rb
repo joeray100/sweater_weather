dacf1f17532ec01781842e1368ac3019cc54573b
Joe Ray <jray100.jr@gmail.com>
2021-08-08 01:08:45 -0600

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:api_key) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_confirmation_of(:password) }
   end
-
-  # describe 'relationships' do
-  #   it { should belong_to(:) }
-  #   it { should have_many(:) }
-  #   it { should have_many(:).through(:) }
-  # end
-  #
-  # before :each do
-  #
-  # end
-  #
-  # describe 'class methods' do
-  #   describe '.' do
-  #   end
-  # end
-  #
-  # describe 'instance methods' do
-  #   describe '#' do
-  #   end
-  # end
 end
