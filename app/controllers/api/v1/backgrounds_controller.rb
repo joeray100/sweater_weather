09742b794e160947f07accefab32e01b57ec6a3f
Joe Ray <jray100.jr@gmail.com>
2021-08-07 20:41:03 -0600

    Update backgrounds controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
+    location = params[:location]
+    image = BackgroundFacade.background_image(location)
+    render json: ImageSerializer.new(image)
   end
 end
