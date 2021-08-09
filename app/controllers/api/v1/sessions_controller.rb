ca823716a1280f40ac4783ff0dfa9ad4d4b9242e
Joe Ray <jray100.jr@gmail.com>
2021-08-08 18:19:51 -0600

    Add error handling conditional to action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::SessionsController < ApplicationController
     if user.authenticate(params[:password]) && user
       session[:user_id] = user.id
       render json: UsersSerializer.new(user)
+    else
+      render json: { error: 'Email or password are invalid' }, status: 406
     end
   end
 end
