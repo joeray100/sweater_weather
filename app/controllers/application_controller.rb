553d3bebd40bda5e94e2aa2e41042932c4420fd5
Joe Ray <jray100.jr@gmail.com>
2021-08-08 18:19:08 -0600

    Remove error handling code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,2 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  def render_unprocessable_entity_response(exception)
-    render json: exception.record.errors, status: :unprocessable_entity
-  end
-
-  def render_not_found_response(exception)
-    render json: { error: exception.message }, status: :not_found
-  end
 end
-
-#
-# rescue_from LocationError::NoLocation, with: :render_not_found_response
-# rescue_from LocationError::InvalidLocation, with: :render_bad_request_response
-#
-# def render_not_found_response(exception)
-#   render json: { error: {message: exception.message, code: exception.code} }, status: exception.status
-# end
-#
-# def render_bad_request_response(exception)
-#   render json: { error: {message: exception.message, code: exception.code} }, status: exception.status
-# end
