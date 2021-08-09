960e4f25b480021edd42156c6773f8f059a32152
Joe Ray <jray100.jr@gmail.com>
2021-08-08 18:19:47 -0600

    Add error handling conditional to action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,13 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.api_key = @key
-    if user.save
+    if user.password.nil? || user.email.empty? || user.password_confirmation.nil?
+      render json: { error: 'Can not register user, please check the required fields' }, status: 400
+    elsif user.password != user.password_confirmation
+      render json: { error: "Can not register user, password and password confirmation don't match" }, status: 400
+    elsif User.find_by(email: user[:email]).present?
+      render json: { error: 'User already exists, please try again' }, status: :not_found
+    elsif user.save
       render json: UsersSerializer.new(user), status: 201
     end
   end
