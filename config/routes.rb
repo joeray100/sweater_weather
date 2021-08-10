aa48b298d7111ca9fc7be745223f9d56a77a077a
Joe Ray <jray100.jr@gmail.com>
2021-08-10 15:38:13 -0600

    Update routes with RT

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'backgrounds#show'
-      resources :sessions, only: :create
       resources :users, only: :create
+      resources :sessions, only: :create
+      resources :road_trip, only: :create
     end
   end
 end
