a3a8ab154a51dccd0edfd2c31b3aff5f0e3cd3c7
Joe Ray <jray100.jr@gmail.com>
2021-08-10 15:39:50 -0600

    Update Directions PORO - had issue with limit on to_time method

--- app/poros/directions.rb
+++ app/poros/directions.rb
@@ -1,15 +1,40 @@
 class Directions
   attr_reader :id,
-              :title,
-              :vote_average,
-              :runtime,
-              :genres
+              :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
 
   def initialize(start_city, end_city, travel_time, weather)
     @id = nil
-    @start_city = poro_info[:start_city]
-    @end_city = poro_info[:end_city]
-    @travel_time = poro_info[:travel_time]
-    @weather_at_eta = poro_info[:weather_at_eta]
+    @start_city = start_city
+    @end_city = end_city
+    @travel_time = create_travel_time(travel_time)
+    @weather_at_eta = create_weather_at_eta(weather, travel_time, end_city)
+  end
+
+  def create_travel_time(travel)
+    code = travel[:info][:statuscode]
+    if code != 0
+      'impossible route'
+    else
+      travel[:route][:formattedTime]
+    end
+  end
+
+  def create_weather_at_eta(weather, travel, end_city)
+    if @travel_time == 'impossible route'
+      {}
+    elsif @travel_time.to_i < 48
+      {
+        temperature: weather[:hourly][@travel_time.to_i][:temp],
+        conditions: weather[:hourly][@travel_time.to_i][:weather][0][:description]
+      }
+    elsif @travel_time.to_time.hour > 48 < 192 # more than 2 less than 8 days
+      {
+        temperature: weather[:daily][@travel_time.to_i/24 - 1][:temp][:day],
+        conditions: weather[:daily][@travel_time.to_i/24 - 1][:weather][0][:description]
+      }
+    end
   end
 end
