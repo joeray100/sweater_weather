3915c5be260f17a3010ece84be518a29a36b54cc
Joe Ray <jray100.jr@gmail.com>
2021-08-11 10:52:10 -0600

    Spacing

--- app/facades/base_facade.rb
+++ app/facades/base_facade.rb
@@ -5,11 +5,13 @@ class BaseFacade
       coordinates = Coordinates.new(lat_lon)
       WeatherService.retrieve_forecast_data(coordinates.latitude, coordinates.longitude)
     end
+    
     def coordinates_for_brewery(location)
       lat_lon = MapQuestService.find_location(location)
       coordinate = Coordinates.new(lat_lon)
       "#{coordinate.latitude}" + ',' + "#{coordinate.longitude}"
     end
+
     def find_weather_for_destination(destination)
       lat_lon = MapQuestService.find_location(destination)
       coordinates = Coordinates.new(lat_lon)
